033fe8f37ce30ec7cbaf1c556a938019d815be8d
KhoiPham2918 <khoikt2006@gmail.com>
2025-12-11 20:53:54 +0700

    Update update_item.py

--- src/menu/update_item.py
+++ src/menu/update_item.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+from src.core.database import load_db, save_db
+
 def update_item(item_id, name=None, price=None, category_id=None):
-    """MN02 – Cập nhật món"""
-    print(f"Cập nhật món {item_id}")
+    items = load_db("menu")
+    for it in items:
+        if int(it["id"]) == int(item_id):
+            if name is not None:
+                it["name"] = str(name)
+            if price is not None:
+                it["price"] = int(price)
+            if category_id is not None:
+                it["category_id"] = int(category_id)
+            save_db("menu", items)
+            print(f"✅ Đã cập nhật món {item_id}")
+            return
+    print("❌ Không tìm thấy món")
