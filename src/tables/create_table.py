a991ffb2631f600fe0850980aec7ae112dbaa8b3
Đường Minh Anh <dminhanh0901@gmail.com>
2025-12-11 15:48:43 +0700

    Update create_table.py

--- src/tables/create_table.py
+++ src/tables/create_table.py
@@ -1,3 +1,15 @@
+# src/tables/create_table.py
+from src.core.database import load_db, save_db, next_id
+
 def create_table(name, seats):
-    """TB01 – Tạo bàn mới"""
-    print(f"Tạo bàn: {name}, số ghế: {seats}")
+    tables = load_db("tables")
+    new_id = next_id("tables")
+    table = {
+        "id": new_id,
+        "name": str(name),
+        "seats": int(seats),
+        "status": "Trống"
+    }
+    tables.append(table)
+    save_db("tables", tables)
+    print(f"✅ Tạo bàn thành công: ID {new_id} | {name} ({seats} ghế)")
