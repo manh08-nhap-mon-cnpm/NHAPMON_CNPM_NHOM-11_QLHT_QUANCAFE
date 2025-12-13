66e8dce8eeba27ad17bb13f13b9481392037b8fd
manh08-nhap-mon-cnpm <dminhanh0901@gmail.com>
2025-12-13 21:36:45 +0700

    update login and dashboard pastel

--- web/app.py
+++ web/app.py
@@ -1,12 +1,25 @@
-import os
-print(os.getcwd())
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = request.form.get("username")
+        pwd = request.form.get("password")
+
+        if user == "admin" and pwd == "123":
+            return redirect(url_for("dashboard"))
+        else:
+            return render_template("login.html", error="❌ Sai tài khoản hoặc mật khẩu")
+
+    return render_template("login.html")
+
+
+@app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
